4824d9e84c2cbdd586ff663ebafb9f6d5c3c5e46
glush <glushkovbogdan@gmail.com>
2024-05-08 19:32:48 +0500

    Ready work v1.3 -- rework system of RMSE values for different models

--- main.py
+++ main.py
@@ -24,17 +24,24 @@ def main():
 
     avg_rms = []
 
-    avg_rms.append(moving_avg_prediction(df))
-    avg_rms.append(linear_regression_prediction(df))
-    avg_rms.append(k_nearest_neighbours_predict(df))
-    avg_rms.append(fb_prophet_prediction(df))
-    avg_rms.append(auto_arima_prediction(df))
-    avg_rms.append(lstm_prediction(df))
+    avg_rms.append([moving_avg_prediction(df), 'Скользящие средние'])
+    avg_rms.append([linear_regression_prediction(df), 'Линейная регрессия'])
+    avg_rms.append([k_nearest_neighbours_predict(df), 'К-ближайщих соседей'])
+    avg_rms.append([fb_prophet_prediction(df), 'Prophet'])
+    avg_rms.append([auto_arima_prediction(df), 'ARIMA'])
+    avg_rms.append([lstm_prediction(df), 'LSTM'])
 
-    models = ['Скользящие средние', 'Линейная регрессия', 'К-ближайщих соседей', 'Prophet', 'ARIMA', 'LSTM']
+    sorted_avg_rms = sorted(avg_rms, key=lambda x: x[0])
+
+    models = []
+    avg_scores = []
+
+    for i in range(len(sorted_avg_rms)):
+        avg_scores.append(sorted_avg_rms[i][0])
+        models.append(sorted_avg_rms[i][1])
 
     # Create a bar plot
-    plt.bar(models, avg_rms)
+    plt.bar(models, avg_scores)
 
     # Add title and labels
     plt.title('RMSE values for different models')
